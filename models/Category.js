d23b28bbaaf3dfdc84afbc0ef573507ced0e0007
Artem Bondarchuk <artem.bondarchuk1233@gmail.com>
2025-11-04 17:07:50 +0200

    Define Category model and associations

---
+++ models/Category.js
@@ -0,0 +1,29 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+    static associate(models) {
+      Category.hasMany(models.Record, {
+        foreignKey: 'category_id'
+      });
+    }
+  }
+  Category.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    }
+  }, {
+    sequelize,
+    modelName: 'Category',
+    timestamps: false // Не створювати createdAt/updatedAt
+  });
+  return Category;
+};
