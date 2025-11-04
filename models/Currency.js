ada9d9be46299a897c120ae87ab4b685991e0286
Artem Bondarchuk <artem.bondarchuk1233@gmail.com>
2025-11-04 17:08:04 +0200

    Define Currency model and associations

---
+++ models/Currency.js
@@ -0,0 +1,32 @@
+// models/Currency.js
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Currency extends Model {
+    static associate(models) {
+
+    }
+  }
+  Currency.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    code: {
+      type: DataTypes.STRING(3),
+      allowNull: false,
+      unique: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Currency',
+    timestamps: false
+  });
+  return Currency;
+};
