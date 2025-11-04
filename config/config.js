16f6cf359bcace67ba96b29dd99dde205878b52e
Artem Bondarchuk <artem.bondarchuk1233@gmail.com>
2025-11-04 21:40:09 +0200

    update config

--- config/config.js
+++ config/config.js
@@ -11,5 +11,13 @@ module.exports = {
   test: {
   },
   production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
