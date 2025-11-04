d65d0fb40ee8de47c887903b4e90c1b246aaa19f
Artem Bondarchuk <artem.bondarchuk1233@gmail.com>
2025-11-04 17:09:35 +0200

    Define User model and associations

---
+++ models/User.js
@@ -0,0 +1,37 @@
+// models/User.js
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      // Юзер має валюту за замовчуванням
+      User.belongsTo(models.Currency, {
+        foreignKey: 'default_currency_id',
+        as: 'defaultCurrency'
+      });
+      
+      // Юзер має багато записів
+      User.hasMany(models.Record, {
+        foreignKey: 'user_id'
+      });
+    }
+  }
+  User.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+    // 'default_currency_id' буде додано міграцією
+  }, {
+    sequelize,
+    modelName: 'User',
+    timestamps: true
+  });
+  return User;
+};
