be7537d89d85f6c6953ca42799a3058aaf556848
Artem Bondarchuk <artem.bondarchuk1233@gmail.com>
2025-11-23 21:15:13 +0200

    Updated User model with authentication fields and password hashing

--- models/User.js
+++ models/User.js
@@ -1,21 +1,25 @@
 'use strict';
 const { Model } = require('sequelize');
+const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
-      // Юзер має валюту за замовчуванням
       User.belongsTo(models.Currency, {
         foreignKey: 'default_currency_id',
         as: 'defaultCurrency'
       });
       
-      // Юзер має багато записів
       User.hasMany(models.Record, {
         foreignKey: 'user_id'
       });
     }
+
+    async comparePassword(candidatePassword) {
+      return await bcrypt.compare(candidatePassword, this.password);
+    }
   }
+  
   User.init({
     id: {
       type: DataTypes.INTEGER,
@@ -25,12 +29,35 @@ module.exports = (sequelize, DataTypes) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
-    // 'default_currency_id' буде додано міграцією
   }, {
     sequelize,
     modelName: 'User',
-    timestamps: true
+    timestamps: true,
+    hooks: {
+      beforeCreate: async (user) => {
+        if (user.password) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      },
+      beforeUpdate: async (user) => {
+        if (user.changed('password')) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      }
+    }
   });
+  
   return User;
 };
